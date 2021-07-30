367892de8f826f89bfedf08dd868f364df4776a5
nulliver <nulliver@xoml.net>
2021-07-30 11:24:52 -0400

    More logging

--- controllers/controller.go
+++ controllers/controller.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 
 	"slackbot-test/services"
@@ -17,27 +18,32 @@ func ProcessEvents(gctx *gin.Context) {
 	body, err := ioutil.ReadAll(gctx.Request.Body)
 	if err != nil {
 		log.Print(err)
+		gctx.String(http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	sv, err := slack.NewSecretsVerifier(gctx.Request.Header, os.Getenv("SLACK_SIGNING_SECRET"))
 	if err != nil {
 		log.Print(err)
+		gctx.String(http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	if _, err := sv.Write(body); err != nil {
 		log.Print(err)
+		gctx.String(http.StatusInternalServerError, err.Error())
 		return
 	}
 	if err := sv.Ensure(); err != nil {
 		log.Print(err)
+		gctx.String(http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
 	if err != nil {
 		log.Print(err)
+		gctx.String(http.StatusInternalServerError, err.Error())
 		return
 	}
 
